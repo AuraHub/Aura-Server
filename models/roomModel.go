f217ad7e557e3b669edf6a0a7da11d2d4dbb57bb
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2024-03-13 10:22:36 +0100

    Update room models

--- models/roomModel.go
+++ models/roomModel.go
@@ -10,7 +10,7 @@ type Room struct {
 	ID        uuid.UUID `gorm:"primaryKey;default:gen_random_uuid();type:uuid;"`
 	Name      string    `gorm:"not null;"`
 	Devices   []Device  `gorm:"foreignKey:RoomID"`
-	Users     []User    `gorm:"many2many:user_rooms;"`
+	CreatedBy uuid.UUID `gorm:"type:uuid;"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime"`
 }
--- models/userModel.go
+++ models/userModel.go
@@ -7,12 +7,12 @@ import (
 )
 
 type User struct {
-	ID        uuid.UUID `gorm:"primaryKey;default:gen_random_uuid();type:uuid;"`
-	Name      string    `gorm:"not null;"`
-	LastName  string    `gorm:"not null;"`
-	Email     string    `gorm:"unique;not null;"`
-	Password  string    `gorm:"not null;"`
-	Rooms     []Room    `gorm:"many2many:user_rooms;"`
-	CreatedAt time.Time `gorm:"autoCreateTime"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+	ID           uuid.UUID `gorm:"primaryKey;default:gen_random_uuid();type:uuid;"`
+	Name         string    `gorm:"not null;"`
+	LastName     string    `gorm:"not null;"`
+	Email        string    `gorm:"unique;not null;"`
+	Password     string    `gorm:"not null;"`
+	CreatedRooms []Room    `gorm:"foreignKey:CreatedBy;"`
+	CreatedAt    time.Time `gorm:"autoCreateTime"`
+	UpdatedAt    time.Time `gorm:"autoUpdateTime"`
 }
